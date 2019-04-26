50f3e893c7f216c9257318eef3ccf4fac8ee0ae1
Rahul <singh.rahul.net@gmail.com>
2019-04-27 06:45:20 +1000

    ocelot POC working

---
+++ Movies.Gateway.Api/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
+
+namespace TicketGateway.Api
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile("ocelot.json")
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot(Configuration)
+                .AddSingletonDefinedAggregator<MovieAggregator>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseOcelot().Wait();
+        }
+    }
+}
