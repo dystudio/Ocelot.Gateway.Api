83fdd3d5e4d72ba9a05a7e0e4fc26d00ff88b007
Rahul <singh.rahul.net@gmail.com>
2019-04-27 15:03:49 +1000

    ocelot returning aggregated records in one shot

--- CheapestMovies.Api/Controllers/MoviesController.cs
+++ CheapestMovies.Api/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Net.Http;
 
 namespace CheapestMovies.Api.Controllers
@@ -13,8 +14,10 @@ namespace CheapestMovies.Api.Controllers
         [HttpGet]
         public ActionResult GetCheapestMovies()
         {
-           
+
             string url = "http://localhost:2000/api/movies";
+            //string url = "http://localhost:2000/api/filmworld/movie/fw0076759";
+            //string url = "http://localhost:2000/api/filmworld/movies";
 
             var response = client.GetAsync(url)
                      .ContinueWith(async x =>
@@ -25,11 +28,48 @@ namespace CheapestMovies.Api.Controllers
 
                          var res = await result.Content.ReadAsStringAsync();
 
-                         return JsonConvert.DeserializeObject<object>(res);
+                         var movies = JsonConvert.DeserializeObject<Dictionary<string, MoviesCollection>>(res);
+                         return movies;
                      }).Result;
 
-            return Ok(response);         
+            return Ok(response.Result);
         }
-       
+        [HttpGet("{id}")]
+        public ActionResult GetMovieDetailById(string id)
+        {
+
+            string url = $"http://localhost:2000/api/movie/{id}";
+
+            var response = client.GetAsync(url)
+                     .ContinueWith(async x =>
+                     {
+                         var result = x.Result;
+
+                         result.EnsureSuccessStatusCode();
+
+                         var res = await result.Content.ReadAsStringAsync();
+
+                         var movies = JsonConvert.DeserializeObject<Dictionary<string, Movie>>(res);
+                         return movies;
+                     }).Result;
+
+            return Ok(response.Result);
+        }
+
+
+
+    }
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string ID { get; set; }
+        public string Type { get; set; }
+        public string Poster { get; set; }
+    }
+
+    public class MoviesCollection
+    {
+        public IEnumerable<Movie> Movies { get; set; }
     }
 }
